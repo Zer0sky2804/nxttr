a0ca09d173e1e091b618aed9a83ea4cb7bcaa679
oskeram.04 <oskeram.04@A222-14>
2022-02-09 09:00:55 +0100

    p2

---
+++ p2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int n = int.Parse(textBox2.Text);
+            int v = 1;
+            for(int p = 1; p <= n; p++)
+            {
+                v *= a;
+            }
+            label1.Text = v.ToString();
+        }
+    }
+}
